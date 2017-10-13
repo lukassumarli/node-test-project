3cde7c47ddfcb06b2d1db17562767c3e837a1b55
Lukas Sumarli <lukassumarli@gmail.com>
2017-10-13 07:17:36 +0000

    Projects page and route

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
     });
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageText: 'Portfolio list here'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Error handling request'
